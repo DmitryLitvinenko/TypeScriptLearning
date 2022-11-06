b93e0228a99acfeafeece5ec5261a694e7788f62
dlitvinenko <dlitvinenko@onairent.live>
2022-11-06 17:36:41 +0200

    adding work with arrats example

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 function getFullName(userEntity: { firstName: string, surName: string }): string {
     return `${userEntity.firstName} ${userEntity.surName}`
 }
+
 const getFullNameArrow = (firstName: string, surname: string): string => {
     return `${firstName} ${surname}`
 }
@@ -11,3 +12,20 @@ const user = {
     age: '30'
 }
 console.log(getFullName(user))
+
+//--------------------------------------------------------------------------------------//
+console.log("work with arrays")
+const skills: string[] = ['Dev', 'DevOps', 'Testing']
+
+console.log("print skills in lowercase: ")
+for (const skill of skills) {
+    console.log(skill.toLocaleLowerCase())
+}
+
+const res = skills
+    .filter((s: string) => s !== 'DevOps')
+    .map(s => s +'! ')
+    .reduce((a, b) => a + b)
+console.log("print result: ")
+console.log(res);
+//result: Dev! Testing!
