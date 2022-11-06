025dfed2d656140784de75fd01c68fc945ae7b3e
dlitvinenko <dlitvinenko@onairent.live>
2022-11-06 17:24:33 +0200

    ignore this commit

--- app.ts
+++ app.ts
@@ -1,16 +1,13 @@
 function getFullName(userEntity: { firstName: string, surName: string }): string {
     return `${userEntity.firstName} ${userEntity.surName}`
 }
-
 const getFullNameArrow = (firstName: string, surname: string): string => {
     return `${firstName} ${surname}`
 }
-
 const user = {
     firstName: 'Dmitry',
     surName: 'Flame',
     city: 'Riga',
     age: '30'
 }
-
 console.log(getFullName(user))
