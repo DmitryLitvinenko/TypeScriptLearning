2153435d84a321a5869b4c5a10d8857070ab009a
dlitvinenko <dlitvinenko@onairent.live>
2022-12-03 15:54:37 +0200

    adding Cart creation lesson

--- app.ts
+++ app.ts
@@ -673,3 +673,78 @@ class Vehicle {
     }
 
 }
+
+//--------------------------------------------------------------------------------------//
+console.log("Create cart lesson")
+
+class Product {
+    constructor(
+        public id: number,
+        public title: string,
+        public price: number
+    ) {
+    }
+}
+
+class Delivery {
+    constructor(
+        date: Date
+    ) {
+    }
+}
+
+class HomeDelivery extends Delivery {
+    constructor(date: Date, public address: string) {
+        super(date);
+    }
+}
+
+class ShopDelivery extends Delivery {
+    constructor(public shopdId: number) {
+        super(new Date);
+    }
+}
+
+type DeliveryOptions = HomeDelivery | ShopDelivery
+
+class Cart {
+    private products: Product[] = []
+    private delivery: DeliveryOptions
+
+    addProduct(product: Product): void {
+        this.products.push(product)
+    }
+
+    deleteProductById(productId: number) {
+        this.products = this.products.filter((p: Product) => p.id !== productId)
+    }
+
+    getSum(): number {
+        return this.products
+            .map((p: Product) => p.price)
+            .reduce((p1: number, p2: number) => p1 + p2)
+    }
+
+    setDelivery(delivery: DeliveryOptions): void {
+        this.delivery = delivery
+    }
+
+    public checkOut() {
+        if (this.products.length == 0) {
+            throw new Error("The cart is empty")
+        }
+        if (!this.delivery) {
+            throw new Error("Delivery is not set")
+        }
+        return {success: true}
+    }
+}
+
+const cart = new Cart()
+cart.addProduct(new Product(1, "Cookies", 10))
+cart.addProduct(new Product(2, "Cake", 20))
+cart.addProduct(new Product(3, "Saslik", 35))
+cart.deleteProductById(1)
+cart.setDelivery(new HomeDelivery(new Date(), "Ropazu Iela 1"))
+console.log(cart.getSum())
+console.log(cart.checkOut())
