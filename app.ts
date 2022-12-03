59108aaf98b89753105bc0c7ea446d0b11b9a418
dlitvinenko <dlitvinenko@onairent.live>
2022-12-03 14:31:52 +0200

    adding Class extends

--- app.ts
+++ app.ts
@@ -556,3 +556,40 @@ class User6 implements IPayable, IDeletable {
     }
 }
 
+//--------------------------------------------------------------------------------------//
+console.log("Class Extends")
+type PaymentStatus1 = 'new' | 'paid'
+
+class Payment1 {
+    id: number
+    status: PaymentStatus1 = 'new'
+
+    constructor(id: number) {
+        this.id = id
+    }
+
+    pay() {
+        this.status = 'paid'
+    }
+}
+
+class PersistedPayment extends Payment1 {
+    databaseId: number
+    paidAt: Date
+
+    constructor() {
+        const id = Math.random();
+        super(id)
+    }
+
+    save() {
+        // save to database
+    }
+
+    override pay(date?: Date) {
+        super.pay()
+        if (date) {
+            this.paidAt = date;
+        }
+    }
+}
