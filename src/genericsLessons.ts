9321ac33b553ab16059aedde362e3273d60898b2
dlitvinenko <dlitvinenko@onairent.live>
2023-01-22 12:45:50 +0200

    Generics lesson: restrictions

--- src/genericsLessons.ts
+++ src/genericsLessons.ts
@@ -86,3 +86,30 @@ const logLine: ILogLine<{ a: number }> = {
         a: 1
     }
 }
+
+console.log("Generics: restrictions")
+
+class Vehicle1 {
+    run: number;
+}
+
+//without generic extend generic to not know from where field run should be taken
+function kmToMiles<T extends Vehicle1>(vehicle: T): T {
+    vehicle.run = vehicle.run / 0.62
+    return vehicle;
+}
+
+class LCV extends Vehicle1 {
+    capacity: number
+}
+
+const vehicle = kmToMiles(new Vehicle1())
+const lcv = kmToMiles(new LCV())
+//-------
+
+//this function is useless, only for example, how to use few generics
+function logId<T extends string | number, Y>(id: T, additionalData: Y): { id: T, data: Y } {
+    console.log(id)
+    console.log(additionalData)
+    return {id, data: additionalData}
+}
