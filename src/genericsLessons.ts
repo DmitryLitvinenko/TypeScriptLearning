480e18bd3a9b9e7cabe78e9c66790394f660adba
dlitvinenko <dlitvinenko@onairent.live>
2023-01-22 12:06:39 +0200

    Generics lesson: functions with generics

--- src/genericsLessons.ts
+++ src/genericsLessons.ts
@@ -11,3 +11,21 @@ const check: Record<string, boolean> = {
     driver: true,
     kpp: false
 }
+
+console.log("Generics: function with generic")
+//1. with generic "T" we allow any type
+function logMiddleWare<T>(data: T): T {
+    console.log(data)
+    return data;
+}
+//2. this type can be setup strictly in next functions
+const result = logMiddleWare<number>(10)
+console.log(result)
+
+//allows to split any array with generics
+function getSplitedHalf<T>(data: Array<T>): Aarray <T> {
+    const l = data.length / 2;
+    return data.splice(0, 1)
+}
+
+getSplitedHalf<number>([1, 2, 3])
