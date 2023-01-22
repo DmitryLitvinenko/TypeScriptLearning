0f41cd1a0b43d424ded344550856677499a16410
dlitvinenko <dlitvinenko@onairent.live>
2023-01-22 14:17:52 +0200

    Generics lesson: generic classes

--- src/genericsLessons.ts
+++ src/genericsLessons.ts
@@ -113,3 +113,31 @@ function logId<T extends string | number, Y>(id: T, additionalData: Y): { id: T,
     console.log(additionalData)
     return {id, data: additionalData}
 }
+
+console.log("Generics: generic classes")
+
+class Resp<D, E> {
+    data?: D
+    error?: E
+
+    constructor(data?: D, error?: E) {
+        if (data) {
+            this.data = data
+        }
+        if (error) {
+            this.error = error
+        }
+    }
+}
+
+const response = new Resp<string, number>('data', 0)
+
+class HTTPResponse<F> extends Resp<string, number> {
+    code: F;
+
+    setCode(code: F) {
+        this.code = code
+    }
+}
+
+const response2 = new HTTPResponse();
